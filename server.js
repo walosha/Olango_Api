dce8b8a8a04040289cf80b85200f2222ca4636f6
Olawale Afuye <walosha@yahoo.com>
2020-03-27 16:00:41 +0100

    added morgan

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
-var AccessToken = require("twilio").jwt.AccessToken;
-var VideoGrant = AccessToken.VoiceGrant;
-var app = express();
+const morgan = require("morgan");
+const AccessToken = require("twilio").jwt.AccessToken;
+const VideoGrant = AccessToken.VoiceGrant;
+const app = express();
 
 require("dotenv").config();
-if (process.env.NODE_ENV === "DEV") {
-  // ... initial code block here
+
+if (process.env.NODE_ENV || "development" === "development") {
+  app.use(morgan("dev"));
 }
 
 // Endpoint to generate access token
 app.get("/token", function(request, response) {
-  var identity = { name: "Olawale afuye", id: 42627272829 };
+  const identity = { name: "Olawale afuye", id: 42627272829 };
 
   // Create an access token which we will sign and return to the client,
   // containing the grant we just created
-  var token = new AccessToken(
+  const token = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_KEY,
     process.env.TWILIO_API_SECRET
@@ -33,6 +35,6 @@ app.get("/token", function(request, response) {
     token: token.toJwt()
   });
 });
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
