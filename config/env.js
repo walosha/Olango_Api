9506b11d83277cac15c77a5901f536036810cfd4
Olawale Afuye <walosha@outlook.com>
2020-05-10 02:11:07 +0100

    added config templete

---
+++ config/env.js
@@ -0,0 +1,15 @@
+// ENVIRONMENT NEEDED TO SET UP THIS APPLICATION
+
+// ACCOUNT_SID=
+// API_KEY=
+// API_KEY_SECRET=
+// outgoingApplicationSid=
+// APP_SID=
+// PUSH_CREDENTIAL_SID=
+// pushCredSid=
+// outgoingAPP_SID=
+// DATABASE_LOCAL=
+// port=
+// JWT_SECRET=
+// JWT_COOKIE_EXPIRES_IN=
+// JWT_EXPIRES_IN=
